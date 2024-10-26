dd2001f53daa67d7c62a44d07014070b394b8004
TheCukitoDev <gabriel.diaz.aguilera@gmail.com>
2024-10-26 14:21:38 +0200

    Add sentry

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,35 @@
+import {withSentryConfig} from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+
+
+org: "adreno-store",
+project: "starter",
+
+
+silent: !process.env.CI,
+
+
+widenClientFileUpload: true,
+
+reactComponentAnnotation: {
+enabled: true,
+},
+
+
+tunnelRoute: "/monitoring",
+
+
+hideSourceMaps: true,
+
+
+disableLogger: true,
+
+
+automaticVercelMonitors: true,
+});
